038659620be8b4c1d986be38452d34aea9bcc443
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-05 01:01:40 +0300

    Set up for Model

---
+++ library/CodegenKit/Languages/Haskell/Files/Model.hs
@@ -0,0 +1,12 @@
+module CodegenKit.Languages.Haskell.Files.Model
+  (
+  )
+where
+
+import qualified Coalmine.MultilineTextBuilder as B
+import qualified Coalmine.Name as Name
+import qualified Coalmine.VectorExtras.Generic as GVec
+import CodegenKit.Prelude
+import qualified TextBuilder as B'
+
+-- *
