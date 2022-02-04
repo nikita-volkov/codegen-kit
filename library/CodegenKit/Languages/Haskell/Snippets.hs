ac7aede52beffd28c0359c78686f60b29729c61e
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-05 02:19:06 +0300

    Implement haddock

--- library/CodegenKit/Languages/Haskell/Snippets.hs
+++ library/CodegenKit/Languages/Haskell/Snippets.hs
@@ -7,11 +7,19 @@ where
 import qualified Coalmine.MultilineTextBuilder as B
 import qualified Coalmine.Name as Name
 import CodegenKit.Prelude
+import qualified Data.Text as Text
 import qualified TextBuilder as B'
 
 haddock :: Text -> B.Builder
-haddock = error "TODO"
+haddock =
+  mappend "-- |"
+    . foldMap (mappend "\n-- ")
+    . fmap fromText
+    . Text.lines
 
+-- |
+-- Multiline Haddock in the prefix position.
+-- I.e., appended with newline.
 haddockWithNewline :: Text -> B.Builder
 haddockWithNewline =
   filtered (not . B.null) (flip mappend "\n") . haddock
