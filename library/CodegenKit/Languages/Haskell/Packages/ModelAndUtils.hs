ba3c52811941f4e9684b212c01d79851f34cfaf1
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-05 03:13:16 +0300

    Set up model and utils package

---
+++ library/CodegenKit/Languages/Haskell/Packages/ModelAndUtils.hs
@@ -0,0 +1,11 @@
+module CodegenKit.Languages.Haskell.Packages.ModelAndUtils where
+
+import qualified Coalmine.MultilineTextBuilder as B
+import qualified CodegenKit.Languages.Haskell.Snippets as Snippets
+import CodegenKit.PackageAssembly
+import CodegenKit.Prelude
+
+-- *
+
+package ns =
+  error "TODO"
