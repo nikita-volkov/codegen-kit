ebf2379ec18b740c917b145599f1ac5692519a20
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-05 22:32:20 +0300

    Set up demo

---
+++ demo/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Coalmine.Prelude
+import qualified CodegenKit.Languages.Haskell.Packages.ModelAndUtils as ModelAndUtils
+import qualified CodegenKit.Packaging as Packaging
+
+-- *
+
+main =
+  Packaging.print $
+    mconcat
+      [ modelAndUtilsPackage
+      ]
+
+-- *
+
+modelAndUtilsPackage =
+  error "TODO"
