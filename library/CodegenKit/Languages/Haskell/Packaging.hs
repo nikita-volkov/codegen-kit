062473328ace9a41ada9176dddc3360c9f223cd6
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-09 20:50:54 +0300

    Add Haskell Package

--- library/CodegenKit/Languages/Haskell/Packaging.hs
+++ library/CodegenKit/Languages/Haskell/Packaging.hs
@@ -2,3 +2,14 @@ module CodegenKit.Languages.Haskell.Packaging where
 
 import qualified Coalmine.Name as Name
 import CodegenKit.Prelude
+
+-- *
+
+-- |
+-- A collection of modules with their requirements for
+-- the package configuration files.
+-- E.g., being exposed or hidden.
+newtype HaskellPackage
+  = HaskellPackage
+      (Acc (FilePath, Text))
+  deriving (Semigroup, Monoid)
