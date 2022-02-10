9e9d6d95d727b2e9bf289245d05e04223401f681
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-10 23:27:03 +0300

    Add signature

--- library/CodegenKit/Languages/Haskell/Dependencies.hs
+++ library/CodegenKit/Languages/Haskell/Dependencies.hs
@@ -3,26 +3,20 @@ module CodegenKit.Languages.Haskell.Dependencies where
 import qualified CodegenKit.Languages.Haskell.Packaging as Packaging
 import CodegenKit.Prelude
 
+base, bytestring, containers, scientific, text, time, uuid, vector :: Packaging.Dependency
 base =
   Packaging.dependency "base" 4 [12] 5 []
-
 bytestring =
   Packaging.dependency "bytestring" 0 [10] 0 [12]
-
 containers =
   Packaging.dependency "containers" 0 [6] 0 [7]
-
 scientific =
   Packaging.dependency "scientific" 0 [3] 0 [4]
-
 text =
   Packaging.dependency "text" 1 [2] 3 []
-
 time =
   Packaging.dependency "time" 1 [9] 2 []
-
 uuid =
   Packaging.dependency "uuid" 1 [3] 2 []
-
 vector =
   Packaging.dependency "vector" 0 [12] 0 [13]
