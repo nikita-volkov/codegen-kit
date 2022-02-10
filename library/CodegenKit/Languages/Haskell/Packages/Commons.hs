ad095c374a4c9566c328271c5a19e1a4f0b11745
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-10 22:07:17 +0300

    Update Commons

--- library/CodegenKit/Languages/Haskell/Packages/Commons.hs
+++ library/CodegenKit/Languages/Haskell/Packages/Commons.hs
@@ -1,6 +1,6 @@
 module CodegenKit.Languages.Haskell.Packages.Commons
   ( -- *
-    operators,
+    basePreludes,
   )
 where
 
@@ -13,9 +13,16 @@ import qualified TextBuilder as B'
 
 -- *
 
-operators :: Packaging.Modules
-operators =
-  Packaging.module_ False "Operators" deps contents
+basePreludes :: Packaging.Modules
+basePreludes =
+  Packaging.inNamespace ["base-preludes"] . mconcat $
+    [ baseOperators,
+      baseDataTypes
+    ]
+
+baseOperators :: Packaging.Modules
+baseOperators =
+  Packaging.module_ False "operators" deps contents
   where
     deps =
       [ Dependencies.base
@@ -85,6 +92,72 @@ operators =
         import qualified Data.Ord
         import qualified Data.Ratio
         import qualified Data.Semigroup
+        import qualified Prelude
+
+      |]
+      where
+        namespace =
+          B.uniline $ B'.intercalate "." $ fmap Name.toUpperCamelCaseTextBuilder nsNameList
+
+baseDataTypes :: Packaging.Modules
+baseDataTypes =
+  Packaging.module_ False "data-types" deps contents
+  where
+    deps =
+      [ Dependencies.base
+      ]
+    contents nsNameList =
+      [i|
+        -- |
+        -- A module that reexports only the data types
+        -- defined across various modules of the \"base\" package.
+        --
+        -- By data types we mean that it is the ones we use
+        -- to define data structures.
+        -- It is not abstraction integration wrappers,
+        -- like 'Data.Semigroup.First'.
+        -- It is not resource types like 'System.IO.Handle'.
+        module $namespace.DataTypes
+          ( -- * From "Prelude"
+            Prelude.Bool (..),
+            Prelude.Char,
+            Prelude.Double,
+            Prelude.Either (..),
+            Prelude.Float,
+            Prelude.Integer,
+            Prelude.Maybe (..),
+            Prelude.String,
+
+            -- * From "Data.Int"
+            Data.Int.Int,
+            Data.Int.Int16,
+            Data.Int.Int32,
+            Data.Int.Int64,
+            Data.Int.Int8,
+
+            -- * From "Data.Word"
+            Data.Word.Word,
+            Data.Word.Word16,
+            Data.Word.Word32,
+            Data.Word.Word64,
+            Data.Word.Word8,
+
+            -- * From "Data.Ratio"
+            Data.Ratio.Rational,
+
+            -- * From "Numeric.Natural"
+            Numeric.Natural.Natural,
+
+            -- * From "Data.List.NonEmpty"
+            Data.List.NonEmpty.NonEmpty (..),
+          )
+        where
+
+        import qualified Data.Int
+        import qualified Data.List.NonEmpty
+        import qualified Data.Ratio
+        import qualified Data.Word
+        import qualified Numeric.Natural
         import qualified Prelude
 
       |]
