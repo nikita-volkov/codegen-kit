d001bad5ae906e76c654a51668aa0997402c1bbb
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-11 16:21:43 +0300

    Update the sample

--- demo/Demo/SamplesFor/ModelAndUtils/Iso8601.hs
+++ demo/Demo/SamplesFor/ModelAndUtils/Iso8601.hs
@@ -10,10 +10,43 @@ iso8601Modules =
     ["our", "demo"]
     [ section
         "ISO-8601"
-        [ product
+        [ sum
+            "calendar-date"
+            [i|
+              - `YYYY-MM-DD` or `YYYYMMDD`
+              - `YYYY-MM` (but not `YYYYMM`)
+              - `--MM-DD` or `--MMDD`
+            |]
+            [ variant "ymd" "" [modelType "Ymd"],
+              variant "ym" "" [modelType "Ym"],
+              variant "md" "" [modelType "Md"]
+            ],
+          sum
+            "ymd"
+            "ISO-8601 Year Month Day for representing a date."
+            [ variant
+                "separated"
+                "YYYY-MM-DD"
+                [ primitiveType "Int",
+                  primitiveType "Int",
+                  primitiveType "Int"
+                ],
+              variant
+                "unseparated"
+                "YYYYMMDD"
+                [ primitiveType "Int",
+                  primitiveType "Int",
+                  primitiveType "Int"
+                ]
+            ],
+          product
             "ymd"
             "ISO-8601 Year Month Day for representing a date."
             [ field
+                "separted"
+                "Whether the format contains dashes"
+                (primitiveType "Bool"),
+              field
                 "year"
                 "Year."
                 (primitiveType "Int"),
@@ -26,23 +59,45 @@ iso8601Modules =
                 "Day."
                 (primitiveType "Int")
             ],
-          sum
-            "calendar-date"
+          product
+            "ym"
+            "Year, month.\n`YYYY-MM`."
+            [ field
+                "year"
+                "Year."
+                (primitiveType "Int"),
+              field
+                "month"
+                "Month."
+                (primitiveType "Int")
+            ],
+          product
+            "md"
             [i|
-              - `YYYY-MM-DD` or `YYYYMMDD`
-              - `YYYY-MM` (but not `YYYYMM`)
-              - `--MM-DD` or `--MMDD`
+              ISO-8601 Month Day for representing a date.
+
+              - `--MM-DD`
+              - `--MMDD`
             |]
-            [ variant "ymd" "" [modelType "Ymd"],
-              variant "ym" "" [modelType "Ym"],
-              variant "md" "" [modelType "Md"]
+            [ field
+                "separted"
+                "Whether the format contains dashes"
+                (primitiveType "Bool"),
+              field
+                "month"
+                "Month."
+                (primitiveType "Int"),
+              field
+                "day"
+                "Day."
+                (primitiveType "Int")
             ],
           sum
             "tz"
-            ""
-            [ variant "gmt" "" [],
-              variant "pst" "" [],
-              variant "msk" "" []
+            "Timezone."
+            [ variant "gmt" "GMT" [],
+              variant "pst" "PST" [],
+              variant "msk" "MSK" []
             ]
         ]
     ]
