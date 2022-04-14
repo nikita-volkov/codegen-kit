5cbb053f11cc0e5b3c3b08d2278e11281ba39597
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-04-14 18:28:27 +0300

    Migrate to newer coalmine

--- library/CodegenKit/Prelude.hs
+++ library/CodegenKit/Prelude.hs
@@ -1,17 +1,10 @@
 module CodegenKit.Prelude
   ( module Exports,
-    filtered,
   )
 where
 
+import Coalmine.BaseExtras.Monoid as Exports
 import Coalmine.Inter as Exports (i)
 import Coalmine.Name as Exports (Name)
 import Coalmine.Prelude as Exports hiding (FilePath, Version)
 import Coalmine.SimplePaths as Exports (DirPath, FilePath)
-
--- *
-
-filtered :: Monoid m => (a -> Bool) -> (a -> m) -> a -> m
-filtered p k x
-  | p x = k x
-  | otherwise = mempty
