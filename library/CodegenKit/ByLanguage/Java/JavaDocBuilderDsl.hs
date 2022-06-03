4d9ff9d4706c56b7b227f6270779b5dfe4e6cc7c
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-03 09:46:29 +0300

    Sketch JavaDocBuilderDsl

--- library/CodegenKit/ByLanguage/Java/JavaDocBuilderDsl.hs
+++ library/CodegenKit/ByLanguage/Java/JavaDocBuilderDsl.hs
@@ -1,5 +1,22 @@
 module CodegenKit.ByLanguage.Java.JavaDocBuilderDsl
   ( -- * --
+    Param,
+    Return,
+    Content,
+    Throws,
+
+    -- * JavaDoc Renderings
+    methodJavaDoc,
+    fieldJavaDoc,
+
+    -- * Content
+    textContent,
+    linkContent,
+    inlineCodeContent,
+    preformattedCodeContent,
+
+    -- * Params
+    param,
   )
 where
 
@@ -9,3 +26,50 @@ import CodegenKit.Prelude
 import qualified Data.HashSet as HashSet
 
 -- * --
+
+data Param
+
+data Return
+
+data Content
+
+data Throws
+
+-- * --
+
+methodJavaDoc :: [Content] -> [Param] -> Maybe Return -> [Throws] -> Builder
+methodJavaDoc =
+  error "TODO"
+
+fieldJavaDoc :: [Content] -> Builder
+fieldJavaDoc contents =
+  methodJavaDoc contents [] Nothing []
+
+-- * Content
+
+textContent :: Text -> Content
+textContent =
+  error "TODO"
+
+linkContent :: Text -> Content
+linkContent =
+  error "TODO"
+
+inlineCodeContent :: Text -> Content
+inlineCodeContent =
+  error "TODO"
+
+preformattedCodeContent :: Text -> Content
+preformattedCodeContent =
+  error "TODO"
+
+-- * Params
+
+param ::
+  -- | Name.
+  Text ->
+  -- | Description.
+  Text ->
+  Param
+param =
+  error "TODO"
