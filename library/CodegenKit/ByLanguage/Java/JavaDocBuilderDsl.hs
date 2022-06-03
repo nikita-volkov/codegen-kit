44b2cd061010bdc505c9573b2595441d316b5f63
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-03 09:29:40 +0300

    Init JavaDoc builder

---
+++ library/CodegenKit/ByLanguage/Java/JavaDocBuilderDsl.hs
@@ -0,0 +1,11 @@
+module CodegenKit.ByLanguage.Java.JavaDocBuilderDsl
+  ( -- * --
+  )
+where
+
+import qualified Coalmine.BaseExtras.List as List
+import Coalmine.MultilineTextBuilder
+import CodegenKit.Prelude
+import qualified Data.HashSet as HashSet
+
+-- * --
