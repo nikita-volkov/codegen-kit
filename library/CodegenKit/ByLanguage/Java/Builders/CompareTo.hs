8f2ff4d72e5b81b703f98964378e17164b778ef2
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-07-22 18:15:57 +0300

    Add optional

--- library/CodegenKit/ByLanguage/Java/Builders/CompareTo.hs
+++ library/CodegenKit/ByLanguage/Java/Builders/CompareTo.hs
@@ -3,3 +3,27 @@ module CodegenKit.ByLanguage.Java.Builders.CompareTo where
 import qualified Coalmine.BaseExtras.List as List
 import Coalmine.MultilineTextBuilder
 import CodegenKit.Prelude
+
+data Component = Component
+  { componentStatements :: Builder -> Builder -> Builder
+  }
+
+optional :: Component -> Component
+optional Component {..} =
+  Component statements
+  where
+    statements leftExp rightExp =
+      [j|
+        if ($leftExp.isPresent()) {
+          if ($rightExp.isPresent()) {
+            $substatements
+          } else return 1;
+        } else {
+          if ($rightExp.isPresent()) return -1;
+        }
+      |]
+      where
+        substatements =
+          componentStatements
+            [j|$leftExp.get()|]
+            [j|$rightExp.get()|]
