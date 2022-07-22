4aa21511aa91bcde21f5997a42b28c5425ff3889
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-07-22 18:33:36 +0300

    Implement more

--- library/CodegenKit/ByLanguage/Java/Builders/CompareTo.hs
+++ library/CodegenKit/ByLanguage/Java/Builders/CompareTo.hs
@@ -8,6 +8,52 @@ data Component = Component
   { componentStatements :: Builder -> Builder -> Builder
   }
 
+-- * General
+
+primitive :: Component
+primitive =
+  Component $ \leftExp rightExp ->
+    [j|
+      status = ($leftExp < $rightExp) ? -1 : (($leftExp == $rightExp) ? 0 : 1);
+      if (status != 0) return status;
+    |]
+
+comparable :: Component
+comparable =
+  Component $ \leftExp rightExp ->
+    [j|
+      status = $leftExp.compareTo($rightExp);
+      if (status != 0) return status;
+    |]
+
+-- * Specific Primitives
+
+boolean :: Component
+boolean =
+  Component $ \leftExp rightExp ->
+    [j|
+      status = Boolean.compare($leftExp, $rightExp);
+      if (status != 0) return status;
+    |]
+
+int :: Component
+int =
+  Component $ \leftExp rightExp ->
+    [j|
+      status = Integer.compare($leftExp, $rightExp);
+      if (status != 0) return status;
+    |]
+
+long :: Component
+long =
+  Component $ \leftExp rightExp ->
+    [j|
+      status = Long.compare($leftExp, $rightExp);
+      if (status != 0) return status;
+    |]
+
+-- * Optionals
+
 optional :: Component -> Component
 optional Component {..} =
   Component statements
