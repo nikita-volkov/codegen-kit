42ce6e257ea452c14eb663e9417247a77e79127e
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-04 18:55:11 +0300

    Sketch HaddockComposer

---
+++ library/CodegenKit/ByLanguage/Haskell/HaddockComposer.hs
@@ -0,0 +1,7 @@
+-- | Composer DSL for Haddock.
+module CodegenKit.ByLanguage.Haskell.HaddockComposer where
+
+import qualified Coalmine.MultilineTextBuilder as B
+import CodegenKit.Prelude
+import qualified Data.Text as Text
+import qualified TextBuilderDev as B'
