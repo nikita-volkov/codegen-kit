ae89f7f7cb0bd84a05722ff538fb00cf280cd318
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-11-09 18:58:10 +0300

    Init ProductType

---
+++ library/CodegenKit/ByLanguage/Haskell/Composers/ProductType.hs
@@ -0,0 +1,5 @@
+module CodegenKit.ByLanguage.Haskell.Composers.ProductType where
+
+import qualified Coalmine.MultilineTextBuilder as Builder
+import qualified CodegenKit.ByLanguage.Haskell.Snippets as Snippets
+import CodegenKit.Prelude hiding (intercalate)
