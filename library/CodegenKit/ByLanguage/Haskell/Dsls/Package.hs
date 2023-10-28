1a1119619faea15ddbafb188625827e64f3fe25c
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-10-28 10:55:28 +0300

    Update

--- library/CodegenKit/ByLanguage/Haskell/Dsls/Package.hs
+++ library/CodegenKit/ByLanguage/Haskell/Dsls/Package.hs
@@ -7,10 +7,12 @@ module CodegenKit.ByLanguage.Haskell.Dsls.Package
 where
 
 import Coalmine.Prelude hiding (Dependencies)
+import CodegenKit.ByLanguage.Haskell.Composers.Exp qualified as Exp
 import CodegenKit.Dependencies (Dependencies)
 import CodegenKit.Dependencies qualified as Dependencies
 import CodegenKit.Versioning (VersionRange)
 import CodegenKit.Versioning qualified as VersionRange
+import Data.Set qualified as Set
 
 toFileset :: Package -> Fileset
 toFileset =
@@ -27,7 +29,6 @@ data Component
 
 data Module = Module
   { extensions :: Set Text,
-    ghc :: VersionRange,
     dependencies :: Dependencies,
     content :: Text
   }
@@ -35,16 +36,77 @@ data Module = Module
 data Import
 
 data Code = Code
-  { ghc :: VersionRange,
-    extensions :: Set Text,
+  { extensions :: Set Text,
     dependencies :: Dependencies,
     -- | Modules that are requested to be imported.
     imports :: Set Text,
     splice :: (Text -> Text) -> Splice
   }
 
-exp :: Exp -> Code
-exp =
-  error "TODO"
+instance Semigroup Code where
+  left <> right =
+    Code
+      { extensions = left.extensions <> right.extensions,
+        dependencies = left.dependencies <> right.dependencies,
+        imports = left.imports <> right.imports,
+        splice = left.splice <> right.splice
+      }
+
+instance Monoid Code where
+  mempty =
+    Code
+      { extensions = mempty,
+        dependencies = mempty,
+        imports = mempty,
+        splice = mempty
+      }
+
+expCode :: Exp -> Code
+expCode exp =
+  Code
+    { extensions = exp.extensions,
+      dependencies = exp.dependencies,
+      imports = exp.imports,
+      splice = Exp.ungroupedExp . exp.baseExp
+    }
+
+-- | For explicitly adding dependencies to code.
+dependenciesCode :: Dependencies -> Code
+dependenciesCode dependencies =
+  Code
+    { extensions = mempty,
+      dependencies,
+      imports = mempty,
+      splice = mempty
+    }
 
 data Exp = Exp
+  { extensions :: Set Text,
+    dependencies :: Dependencies,
+    -- | Modules that are requested to be imported.
+    imports :: Set Text,
+    baseExp :: (Text -> Text) -> Exp.Exp
+  }
+
+refExp ::
+  Maybe Dependency ->
+  -- | Module name.
+  Text ->
+  -- | Symbol name.
+  Text ->
+  Exp
+refExp dependency moduleName symbolName =
+  Exp
+    { extensions = mempty,
+      dependencies = foldMap (.dependencies) dependency,
+      imports = Set.singleton moduleName,
+      baseExp = \deref -> Exp.reference (deref moduleName) symbolName
+    }
+
+data Dependency = Dependency
+  { dependencies :: Dependencies
+  }
+
+dependency :: Text -> VersionRange -> Dependency
+dependency packageName versionRange =
+  error "TODO"
