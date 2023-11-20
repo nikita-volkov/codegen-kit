8e6c150a3918fbaed29388394580107e6a2fdb02
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-11-21 01:48:40 +0300

    Sketch comonad code context

---
+++ packages/haskell-package/library/CodegenKit/HaskellPackage/Aggregates/CodeRequirements.hs
@@ -0,0 +1,122 @@
+module CodegenKit.HaskellPackage.Aggregates.CodeRequirements
+  ( CodeRequirements (..),
+    fromSplice,
+    fromSymbolImport,
+    fromModuleImport,
+    fromExtension,
+    fromDependency,
+    addSymbolImport,
+    addModuleImport,
+  )
+where
+
+import Coalmine.EvenSimplerPaths qualified as Path
+import Coalmine.Fileset qualified as Fileset
+import Coalmine.MultilineTextBuilder qualified as Splice
+import Coalmine.Prelude
+import CodegenKit.HaskellPackage.Contexts.Package qualified as Package
+import CodegenKit.Legacy.ByLanguage.Haskell.CodeTemplate qualified as CodeTemplate
+import CodegenKit.Legacy.ByLanguage.Haskell.Templates.ImportsBlock qualified as ImportsBlockTemplate
+import CodegenKit.Legacy.Dependencies qualified as Dependencies
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
+import Data.Text qualified as Text
+
+-- | Compiled code with metadata.
+data CodeRequirements = CodeRequirements
+  { extensions :: Set Text,
+    dependencies :: Dependencies.Dependencies,
+    -- | Modules and symbols that are requested to be imported.
+    symbolImports :: Map Text (Set Text),
+    moduleImports :: Set Text
+  }
+
+instance Semigroup CodeRequirements where
+  left <> right =
+    CodeRequirements
+      { extensions = left.extensions <> right.extensions,
+        dependencies = left.dependencies <> right.dependencies,
+        symbolImports = Map.unionWith Set.union left.symbolImports right.symbolImports,
+        moduleImports = Set.union left.moduleImports right.moduleImports
+      }
+
+instance Monoid CodeRequirements where
+  mempty =
+    CodeRequirements
+      { extensions = mempty,
+        dependencies = mempty,
+        symbolImports = mempty,
+        moduleImports = mempty
+      }
+
+fromSplice :: Splice -> CodeRequirements
+fromSplice splice =
+  CodeRequirements
+    { extensions = mempty,
+      dependencies = mempty,
+      symbolImports = mempty,
+      moduleImports = mempty
+    }
+
+fromSymbolImport :: Text -> Text -> CodeRequirements
+fromSymbolImport moduleName symbolName =
+  CodeRequirements
+    { extensions = mempty,
+      dependencies = mempty,
+      symbolImports = Map.singleton moduleName (Set.singleton symbolName),
+      moduleImports = mempty
+    }
+
+fromModuleImport :: Text -> CodeRequirements
+fromModuleImport moduleName =
+  CodeRequirements
+    { extensions = mempty,
+      dependencies = mempty,
+      symbolImports = mempty,
+      moduleImports = Set.singleton moduleName
+    }
+
+fromExtension :: Text -> CodeRequirements
+fromExtension extension =
+  CodeRequirements
+    { extensions = Set.singleton extension,
+      dependencies = mempty,
+      symbolImports = mempty,
+      moduleImports = mempty
+    }
+
+fromDependency :: Text -> Word -> [Word] -> Word -> [Word] -> CodeRequirements
+fromDependency packageName minHead minTail maxHead maxTail =
+  CodeRequirements
+    { extensions = mempty,
+      dependencies = Dependencies.singleton packageName minHead minTail maxHead maxTail,
+      symbolImports = mempty,
+      moduleImports = mempty
+    }
+
+mapSymbolImports :: (Map Text (Set Text) -> Map Text (Set Text)) -> CodeRequirements -> CodeRequirements
+mapSymbolImports mapper compiledCode =
+  compiledCode
+    { symbolImports = mapper compiledCode.symbolImports
+    }
+
+mapModuleImports :: (Set Text -> Set Text) -> CodeRequirements -> CodeRequirements
+mapModuleImports mapper compiledCode =
+  compiledCode
+    { moduleImports = mapper compiledCode.moduleImports
+    }
+
+addSymbolImport :: Text -> Text -> CodeRequirements -> CodeRequirements
+addSymbolImport moduleName symbolName =
+  mapSymbolImports
+    $ Map.alter
+      ( \case
+          Nothing -> Just (Set.singleton symbolName)
+          Just set -> Just (Set.insert symbolName set)
+      )
+      moduleName
+
+addModuleImport :: Text -> CodeRequirements -> CodeRequirements
+addModuleImport moduleName =
+  mapModuleImports
+    $ Set.insert moduleName
---
+++ packages/haskell-package/library/CodegenKit/HaskellPackage/Contexts/Code2.hs
@@ -0,0 +1,63 @@
+module CodegenKit.HaskellPackage.Contexts.Code2 where
+
+import Coalmine.EvenSimplerPaths qualified as Path
+import Coalmine.Fileset qualified as Fileset
+import Coalmine.MultilineTextBuilder qualified as Splice
+import Coalmine.Prelude hiding (exp)
+import CodegenKit.HaskellPackage.Aggregates.CodeRequirements (CodeRequirements)
+import CodegenKit.HaskellPackage.Aggregates.CodeRequirements qualified as CodeRequirements
+import CodegenKit.HaskellPackage.Contexts.Package qualified as Package
+import CodegenKit.Legacy.ByLanguage.Haskell.CodeTemplate qualified as CodeTemplate
+import CodegenKit.Legacy.ByLanguage.Haskell.Templates.ImportsBlock qualified as ImportsBlockTemplate
+import CodegenKit.Legacy.Dependencies qualified as Dependencies
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
+import Data.Text qualified as Text
+import TextBuilderDev qualified as UnilineSplice
+import "comonad" Control.Comonad
+import "comonad" Control.Comonad.Env
+import "comonad" Control.Comonad.Store
+import "comonad" Control.Comonad.Traced
+
+newtype Config = Config
+  { -- | Function attempting to look up an alias or
+    -- qualified reference for a module,
+    -- producing empty text otherwise.
+    aliasModule :: Text -> Text
+  }
+
+instance Semigroup Config where
+  left <> right =
+    Config
+      { aliasModule = \input ->
+          let leftResult = left.aliasModule input
+           in if Text.null leftResult
+                then right.aliasModule input
+                else leftResult
+      }
+
+instance Monoid Config where
+  mempty =
+    Config
+      { aliasModule = const Text.empty
+      }
+
+newtype Context a = Context
+  { envt :: EnvT CodeRequirements ((->) Config) a
+  }
+
+deriving instance Functor Context
+
+deriving instance Applicative Context
+
+instance Comonad Context where
+  extract = extract . (.envt)
+  duplicate (Context envt) = unsafeCoerce (duplicate envt)
+
+deriving instance ComonadEnv CodeRequirements Context
+
+deriving instance ComonadTraced Config Context
+
+context :: CodeRequirements -> (Config -> a) -> Context a
+context codeRequirements compile =
+  Context (EnvT codeRequirements compile)
