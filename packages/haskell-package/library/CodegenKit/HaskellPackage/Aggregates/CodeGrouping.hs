486574fb19b45d2a207cc8479ef844df52d060b5
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-11-21 02:51:51 +0300

    Add CodeGrouping Aggregate

---
+++ packages/haskell-package/library/CodegenKit/HaskellPackage/Aggregates/CodeGrouping.hs
@@ -0,0 +1,33 @@
+module CodegenKit.HaskellPackage.Aggregates.CodeGrouping where
+
+import Coalmine.EvenSimplerPaths qualified as Path
+import Coalmine.Fileset qualified as Fileset
+import Coalmine.MultilineTextBuilder qualified as Splice
+import Coalmine.Prelude
+import CodegenKit.HaskellPackage.Contexts.Package qualified as Package
+import CodegenKit.Legacy.ByLanguage.Haskell.CodeTemplate qualified as CodeTemplate
+import CodegenKit.Legacy.ByLanguage.Haskell.Templates.ImportsBlock qualified as ImportsBlockTemplate
+import CodegenKit.Legacy.Dependencies (Dependencies)
+import CodegenKit.Legacy.Dependencies qualified as Dependencies
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
+import Data.Text qualified as Text
+
+data CodeGrouping = CodeGrouping
+  { needsGrouping :: Bool,
+    isMultiline :: Bool
+  }
+
+instance Semigroup CodeGrouping where
+  left <> right =
+    CodeGrouping
+      { needsGrouping = left.needsGrouping || right.needsGrouping,
+        isMultiline = left.isMultiline || right.isMultiline
+      }
+
+instance Monoid CodeGrouping where
+  mempty =
+    CodeGrouping
+      { needsGrouping = False,
+        isMultiline = False
+      }
