66280690782dc405eafa7cbe4310925950e6d069
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-12-02 20:17:45 +0300

    Redesign and implement Exp

--- packages/haskell-package-v2/library/CodegenKit/HaskellPackage/Dsls/Exp.hs
+++ packages/haskell-package-v2/library/CodegenKit/HaskellPackage/Dsls/Exp.hs
@@ -22,33 +22,108 @@ import TextBuilderDev qualified as UnilineSplice
 
 toGroupedCodeSplice :: Exp -> Code.Context Splice
 toGroupedCodeSplice x =
-  if x.codeGrouping.needsGrouping
+  if x.needsGrouping
     then
-      if x.codeGrouping.isMultiline
-        then "( " <> fmap (Splice.indent 2) x.codeSplice <> "\n)"
-        else "(" <> x.codeSplice <> ")"
-    else x.codeSplice
+      if x.isMultiline
+        then "( " <> fmap (Splice.indent 2) x.content <> "\n)"
+        else "(" <> x.content <> ")"
+    else x.content
 
 toUngroupedCodeSplice :: Exp -> Code.Context Splice
-toUngroupedCodeSplice = (.codeSplice)
+toUngroupedCodeSplice x =
+  x.content
 
 data Exp = Exp
-  { codeGrouping :: CodeGrouping.CodeGrouping,
-    codeSplice :: Code.Context Splice
+  { needsGrouping :: Bool,
+    isMultiline :: Bool,
+    content :: Code.Context Splice
   }
+
+localSymbol :: Text -> Exp
+localSymbol text =
+  Exp
+    { needsGrouping = False,
+      isMultiline = False,
+      content = pure (from text)
+    }
+
+-- | Produce code with a symbol reference that is determined based on the imports and requirements for them.
+importedSymbol ::
+  -- | Fully qualified module reference.
+  Text ->
+  -- | Member name.
+  Text ->
+  Exp
+importedSymbol moduleRef memberName =
+  Exp
+    { needsGrouping = False,
+      isMultiline = False,
+      content = Code.importedSymbol moduleRef memberName
+    }
 
 chainApplication :: Exp -> [Exp] -> Exp
 chainApplication function params =
-  if all (not . (.codeGrouping.isMultiline)) params && not function.codeGrouping.isMultiline
+  if all (not . (.isMultiline)) params && not function.isMultiline
     then
       Exp
-        { codeGrouping =
-            CodeGrouping.CodeGrouping
-              { needsGrouping = True,
-                isMultiline = False
-              },
-          codeSplice =
+        { needsGrouping = True,
+          isMultiline = False,
+          content =
             toUngroupedCodeSplice function
               <> foldMap (mappend " " . toGroupedCodeSplice) params
         }
-    else error "TODO"
+    else
+      Exp
+        { needsGrouping = True,
+          isMultiline = False,
+          content =
+            toGroupedCodeSplice function
+              <> fmap (Splice.indent 2) (foldMap (mappend "\n" . toGroupedCodeSplice) params)
+        }
+
+binApplication :: Exp -> Exp -> Exp
+binApplication function param =
+  if function.isMultiline && param.isMultiline
+    then
+      Exp
+        { needsGrouping = True,
+          isMultiline = True,
+          content =
+            function.content <> fmap (Splice.indent 2) ("\n" <> toGroupedCodeSplice param)
+        }
+    else
+      Exp
+        { needsGrouping = True,
+          isMultiline = False,
+          content =
+            function.content <> " " <> toGroupedCodeSplice param
+        }
+
+stringLiteral :: Text -> Exp
+stringLiteral text =
+  Exp False isMultiline (pure splice)
+  where
+    isMultiline =
+      case processedLines of
+        [] -> False
+        [_] -> False
+        _ -> True
+    splice =
+      mconcat
+        [ "\"",
+          Splice.intercalate "\\n\\\n\\" processedLines,
+          "\""
+        ]
+    processedLines =
+      fmap (from @Text)
+        . Text.lines
+        . Text.pack
+        . join
+        . fmap escapeChar
+        . Text.unpack
+        $ text
+      where
+        escapeChar = \case
+          '\\' -> "\\\\"
+          '"' -> "\\\""
+          a -> [a]
