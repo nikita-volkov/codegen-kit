a01a493d19d3e49a4acad313d2bcc3eb680651c4
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-12-03 00:31:20 +0300

    Add tests

---
+++ packages/haskell-package-v2/demo/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import Coalmine.Prelude
+
+main :: IO ()
+main = return ()
---
+++ packages/haskell-package-v2/test/CodegenKit/HaskellPackage/Dsls/ExpSpec.hs
@@ -0,0 +1,9 @@
+module CodegenKit.HaskellPackage.Dsls.ExpSpec where
+
+import Coalmine.Prelude
+import CodegenKit.HaskellPackage.Dsls.Exp
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  return ()
